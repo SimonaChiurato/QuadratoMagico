db66c234f4c75a8acacb239635ce243c255dfbfa
Simona <Simona@LAPTOP-2JANVKQT.homenet.telecomitalia.it>
2020-04-22 11:22:04 +0200

    Esercizio implementato

--- src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class TestRisolviQuadrato {
 
 	public static void main(String args[]) {
-		RisolviQuadrato r = new RisolviQuadrato(3) ;
-		List<List<Integer>> soluzioni = r.risolvi() ;
-		for(List<Integer> sol: soluzioni)
+		RisolviQuadrato r = new RisolviQuadrato(4) ;
+		//r.quadrati();
+		List<List<Integer>> soluzioni = r.quadrati() ;
+	for(List<Integer> sol: soluzioni) {
 			System.out.println(sol) ;
+	}
 	}
 	
 }
